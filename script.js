9c2840f97032ac7497cf6659048fb68fa81dd3b6
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-09-18 00:42:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -18,33 +18,54 @@ function addToList() {
         userInput.value = ''
         saveData();
     }
+    added();
 }
+document.addEventListener('keyup', (e) => {
+    if (e.key == 'Enter') {
+        if (userInput.value === '') {
+            alert('Enter something')
+        } else {
+            let listItem = document.createElement('li')
+            listItem.classList.add('list')
+            listItem.innerHTML = userInput.value
+            taskList.appendChild(listItem)
+            container.classList.add('task-added');
+            listArray.push(listItem)
+            let cross = document.createElement('span')
+            cross.innerHTML = '&Cross;'
+            listItem.appendChild(cross)
+            userInput.value = ''
+            saveData();
+        }
+    }
+})
 
 document.addEventListener('click', (e) => {
     if (e.target.tagName == 'LI') {
         e.target.classList.toggle('checked')
         saveData();
-    }else if (e.target.tagName == 'SPAN') {
+    } else if (e.target.tagName == 'SPAN') {
         e.target.parentElement.remove()
         listArray.pop()
-        saveData(); 
+        saveData();
     }
 })
 document.addEventListener('touchup', (e) => {
     if (e.target.tagName == 'LI') {
         e.target.classList.toggle('checked')
         saveData();
-    }else if (e.target.tagName == 'SPAN') {
+    } else if (e.target.tagName == 'SPAN') {
         e.target.parentElement.remove()
         listArray.pop()
-        saveData(); 
+        saveData();
     }
 })
 
-function saveData(){
-    localStorage.setItem('data',taskList.innerHTML)
+
+function saveData() {
+    localStorage.setItem('data', taskList.innerHTML)
 }
-function showData(){
+function showData() {
     taskList.innerHTML = localStorage.getItem('data')
 }
 showData();
